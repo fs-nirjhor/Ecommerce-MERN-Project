479e4b8adb24a4ceee761bd0806c4e3d82988414
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-09-28 15:53:39 +0600

    data added

---
+++ server/src/data.js
@@ -0,0 +1,20 @@
+const data = {
+  users: [
+    {
+      name: "John",
+      email: "john@example.com",
+      password: "1234",
+      address: "Dhaka",
+      phone: "0147544545",
+    },
+    {
+      name: "jim",
+      email: "jim@example.com",
+      password: "5678",
+      address: "Khulna",
+      phone: "01484545405",
+    },
+  ],
+};
+
+module.exports = data;
