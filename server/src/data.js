9a5a6b37a1046bae8ae1c6afcdb57797b7754f1c
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-09-29 14:53:10 +0600

    data changed

--- server/src/data.js
+++ server/src/data.js
@@ -1,19 +1,147 @@
+// random user data
+
 const data = {
   users: [
     {
-      name: "John",
-      email: "john@example.com",
+      name: "Sharon Brown",
+      email: "sharon.brown@example.com",
       password: "1234",
       address: "Dhaka",
-      phone: "0147544545",
+      phone: "01712345678"
     },
     {
-      name: "jim",
-      email: "jim@example.com",
+      name: "Michael Johnson",
+      email: "michael.johnson@example.com",
+      password: "5678",
+      address: "Chittagong",
+      phone: "01823456789"
+    },
+    {
+      name: "Jennifer Davis",
+      email: "jennifer.davis@example.com",
+      password: "4321",
+      address: "Sylhet",
+      phone: "01934567890"
+    },
+    {
+      name: "David Smith",
+      email: "david.smith@example.com",
+      password: "9876",
+      address: "Khulna",
+      phone: "01645678901"
+    },
+    {
+      name: "Linda Wilson",
+      email: "linda.wilson@example.com",
+      password: "6543",
+      address: "Rajshahi",
+      phone: "01556789012"
+    },
+    {
+      name: "William Taylor",
+      email: "william.taylor@example.com",
+      password: "3456",
+      address: "Barishal",
+      phone: "01467890123"
+    },
+    {
+      name: "Patricia Martinez",
+      email: "patricia.martinez@example.com",
+      password: "7890",
+      address: "Dhaka",
+      phone: "01778901234"
+    },
+    {
+      name: "John Davis",
+      email: "john.davis@example.com",
+      password: "2109",
+      address: "Chittagong",
+      phone: "01889012345"
+    },
+    {
+      name: "Mary Johnson",
+      email: "mary.johnson@example.com",
+      password: "5432",
+      address: "Sylhet",
+      phone: "01990123456"
+    },
+    {
+      name: "Robert Anderson",
+      email: "robert.anderson@example.com",
+      password: "9012",
+      address: "Khulna",
+      phone: "01601234567"
+    },
+    {
+      name: "Karen Wilson",
+      email: "karen.wilson@example.com",
+      password: "7890",
+      address: "Rajshahi",
+      phone: "01512345678"
+    },
+    {
+      name: "Joseph Taylor",
+      email: "joseph.taylor@example.com",
+      password: "4567",
+      address: "Barishal",
+      phone: "01423456789"
+    },
+    {
+      name: "Nancy Martinez",
+      email: "nancy.martinez@example.com",
+      password: "2345",
+      address: "Dhaka",
+      phone: "01734567890"
+    },
+    {
+      name: "Charles Davis",
+      email: "charles.davis@example.com",
+      password: "6789",
+      address: "Chittagong",
+      phone: "01845678901"
+    },
+    {
+      name: "Sarah Johnson",
+      email: "sarah.johnson@example.com",
+      password: "8901",
+      address: "Sylhet",
+      phone: "01956789012"
+    },
+    {
+      name: "Richard Smith",
+      email: "richard.smith@example.com",
       password: "5678",
       address: "Khulna",
-      phone: "01484545405",
+      phone: "01667890123"
     },
+    {
+      name: "Laura Wilson",
+      email: "laura.wilson@example.com",
+      password: "1098",
+      address: "Rajshahi",
+      phone: "01578901234"
+    },
+    {
+      name: "Christopher Taylor",
+      email: "christopher.taylor@example.com",
+      password: "4567",
+      address: "Barishal",
+      phone: "01490123456"
+    },
+    {
+      name: "Elizabeth Martinez",
+      email: "elizabeth.martinez@example.com",
+      password: "3456",
+      address: "Dhaka",
+      phone: "01701234567"
+    },
+    {
+      name: "Daniel Davis",
+      email: "daniel.davis@example.com",
+      password: "8901",
+      address: "Chittagong",
+      phone: "01812345678"
+    }
   ],
 };
 
