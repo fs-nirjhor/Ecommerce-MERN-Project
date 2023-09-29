1b3c102851af24fba26bb4e0e1137c3456b210a7
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-09-29 20:20:39 +0600

    search, limit, pagination

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,12 +1,39 @@
-const users = require("../models/userModel")
-// const createError = require("http-errors");
+//const users = require("../models/userModel")
+const createError = require("http-errors");
 
-const getUsers = (req, res, next) => {
+const User = require("../models/userModel");
+
+const getUsers = async (req, res, next) => {
   try {
-    return res.status(200).json(users)
+    const search = req.query.search || "";
+    const limit = Number(req.query.limit) || 5;
+    const page = Number(req.query.page) || 1;
+    const userRegExp = new RegExp(".*" + search + ".*", "i");
+    const filter = {
+      isAdmin: { $ne: true }, // not equal
+      $or: [
+        // multiple filters
+        { name: { $regex: userRegExp } },
+        { email: { $regex: userRegExp } },
+        { phone: { $regex: userRegExp } },
+      ],
+    };
+    const options = { password: 0 }; // not include
+    const users = await User.find(filter, options)
+      .limit(limit)
+      .skip((page - 1) * limit);
+    const count = await User.find(filter).countDocuments();
+    const pagination = {
+      total: Math.ceil(count / limit),
+      current: page,
+      previous: page - 1 > 0 ? page - 1 : null,
+      next: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+    };
+    if(!users.length) throw createError(404, 'No users found');
+     res.status(200).json({users, pagination});
   } catch (error) {
-    next(error) // next refers to error handler.
+    next(error); // next refers to error handler.
   }
-}
+};
 
-module.exports = getUsers
+module.exports = getUsers;
