69ac7d1a77b6f30a4c23fc698d069dca97710430
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-10-13 15:13:48 +0600

    i cp

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -10,17 +10,22 @@ const { request } = require("express");
 const handleLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
+    //? is email and password provided 
     if (!email || !password) {throw createHttpError(403, 'Please input your email and password');}
+
     //? is user exist
     const user = await findOneItem(User, {email});
+
     //? is password matched
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         throw createHttpError(403, "Wrong password")
     }
+
     //? is user banned
     if (user.isBanned) {throw createHttpError(403, "This account is banned");}
-
+    
     return successResponse(res, {
       statusCode: 200,
       message: "Logged in successfully",
