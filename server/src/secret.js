13eaf326d8f6f9bb0cff6e0892828b4d23313639
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-10-13 15:42:08 +0600

    jwtAccessKey

--- server/src/secret.js
+++ server/src/secret.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 const serverPort = process.env.SERVER_PORT || 3002;
 const databaseUrl =
   process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerceMernDB";
-const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || "abcd1234@#$%^&*";
+const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || "&JwT-@cT!^atiON_keY$";
+const jwtAccessKey = process.env.JWT_ACCESS_KEY || "&JwT-@ccESs_keY$";
 const smtpUser = process.env.SMTP_USER || "";
 const smtpPassword = process.env.SMTP_PASSWORD || "";
 const clientUrl = process.env.CLIENT_URL || "";
@@ -14,6 +15,7 @@ module.exports = {
   serverPort,
   databaseUrl,
   jwtActivationKey,
+  jwtAccessKey,
   smtpUser,
   smtpPassword,
   clientUrl,
