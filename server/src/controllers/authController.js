35133ca146e69a643b2605cff8ea28fbebfaa688
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-10-13 18:59:57 +0600

    set cookie

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,36 +2,47 @@ const User = require("../models/userModel");
 const { successResponse } = require("./responseController");
 const { findOneItem } = require("../services/findItem");
 const { createJwt } = require("../helper/manageJWT");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const createHttpError = require("http-errors");
-const { request } = require("express");
+const { jwtAccessKey } = require("../secret");
 
 const handleLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    //? is email and password provided 
-    if (!email || !password) {throw createHttpError(403, 'Please input your email and password');}
+    //? is email and password provided
+    if (!email || !password) {
+      throw createHttpError(403, "Please input your email and password");
+    }
 
     //? is user exist
-    const user = await findOneItem(User, {email});
+    const user = await findOneItem(User, { email });
 
     //? is password matched
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        throw createHttpError(403, "Wrong password")
+      throw createHttpError(403, "Wrong password");
     }
 
     //? is user banned
-    if (user.isBanned) {throw createHttpError(403, "This account is banned");}
+    if (user.isBanned) {
+      throw createHttpError(403, "This account is banned");
+    }
 
     // create access token
-    const accessToken = createJwt({email}, )
+    const accessToken = createJwt({ email }, jwtAccessKey, "15m");
+
+    // set cookie
+    res.cookie("access_token", accessToken, {
+      maxAge: 15 * 60 * 1000, // 15 minute
+      httpOnly: true,
+      secure: true,
+      sameSite: "none", // call from multiple addresses
+    });
     return successResponse(res, {
       statusCode: 200,
       message: "Logged in successfully",
-      payload: { user },
+      payload: { user, accessToken },
     });
   } catch (error) {
     next(error);
