b884f6c5066d7e150f8dfa2ffce0c6ca9388e14e
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-10-19 10:17:34 +0600

    seed isadmin

--- server/src/routers/seedRouter.js
+++ server/src/routers/seedRouter.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const seedUser = require('../controllers/seedController');
+const { isLoggedIn, isAdmin } = require('../middlewares/auth');
 const seedRouter = express.Router();
 
 // api/seed/users
-seedRouter.post('/users', seedUser)
+seedRouter.post('/users', isLoggedIn, isAdmin, seedUser)
 
 module.exports = seedRouter;
