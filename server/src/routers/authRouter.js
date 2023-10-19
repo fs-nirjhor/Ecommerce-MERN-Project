96a40840f25ee3e01935f0487d053ded7df07687
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-10-19 11:22:53 +0600

    login validation

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const { handleLogin, handleLogout } = require("../controllers/authController");
 const { isLoggedIn, isLoggedOut } = require("../middlewares/auth");
+const { validateUserLogin } = require("../validators/userValidator");
+const runValidations = require("../validators");
 const authRouter = express.Router();
 
 // api/auth/login
-authRouter.post('/login', isLoggedOut, handleLogin);
+authRouter.post('/login', isLoggedOut, validateUserLogin, runValidations, handleLogin);
 authRouter.post('/logout', isLoggedIn, handleLogout);
 
 
