8d21e27f9cb1d0f4e4312521ec6694fd6aa9e08d
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-11-09 12:55:01 +0600

    winston logger setup

--- server/src/helper/winstonLogger.js
+++ server/src/helper/winstonLogger.js
@@ -1,19 +1,28 @@
-const { createLogger, format, transports } = require("winston");
-const {combine, colorize, simple, json, timestamp} = format;
-const logger = createLogger({
+const { createLogger, format, transports} = require("winston");
+const { combine, colorize, simple, timestamp, prettyPrint } = format;
+ 
+const infoTransport = new transports.File({
+  filename: "src/logs/combined.log",
   level: "info",
   format: combine(
-      json(),
-    timestamp({ format: "DD-MM-YYYY HH:MM:SS" }),
+    timestamp({ format: "DD-MM-YYYY hh:mm:ss" }),
+    prettyPrint()
   ),
-  transports: [
-    new transports.Console({
-      format: combine(
-        colorize(),
-        simple(),
-      ),
-    }),
-  ],
+});
+const errorTransport = new transports.File({
+  filename: "src/logs/error.log",
+  level: "error",
+  format: combine(
+    timestamp({ format: "DD-MM-YYYY hh:mm:ss" }),
+    prettyPrint()
+  ),
+});
+const consoleTransport = new transports.Console({
+  format: combine(colorize(), simple()),
+});
+ 
+const logger = createLogger({
+  transports: [consoleTransport, infoTransport, errorTransport],
 });
 
 module.exports = logger;
