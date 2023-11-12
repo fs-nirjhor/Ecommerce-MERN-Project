4b28872ad16cbd7eb3cca2382f6e0f1cf52255e9
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-11-12 12:51:57 +0600

    slugify installed

---
+++ server/src/controllers/categoryController.js
@@ -0,0 +1,15 @@
+const handleCreateCategory = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+
+    return successResponse(res, {
+      statusCode: 201,
+      message: "Category created successfully",
+      payload: {name},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { handleCreateCategory };
