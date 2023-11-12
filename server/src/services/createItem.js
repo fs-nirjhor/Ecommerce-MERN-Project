eeb07e9a8a0acd68de76a0e1dfc7a19ad948cc75
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-11-12 18:31:49 +0600

    handle duplicate collection

--- server/src/services/createItem.js
+++ server/src/services/createItem.js
@@ -8,20 +8,19 @@ const createItem = async (Model, data) => {
         `Please provide data as object to create ${Model.modelName}`
       );
     }
-    const checkedItem = await Model.findOne(data);
-    if (checkedItem) {
-      throw createHttpError(
-        409,
-        `This ${Model.modelName} is already created.`
-      );
-    }
     const item = await Model.create(data);
     return item;
   } catch (error) {
-    // handle mongoose error
-    if (error instanceof mongoose.Error) {
-      throw createHttpError(400, `Failed to create ${Model.modelName}.`);
-    }
+      // handle mongoose error
+      if (error.code === 11000) {
+        throw createHttpError(
+          409,
+          `This ${Model.modelName} is already created.`
+        );
+      }
+        if (error instanceof mongoose.Error) {
+          throw createHttpError(400, `Failed to create ${Model.modelName}.`);
+        }
     throw error;
   }
 };
