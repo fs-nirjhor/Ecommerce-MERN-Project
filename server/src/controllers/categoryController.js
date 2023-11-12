516f1659f47feccadfa812e24219899e8d7dd2d9
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-11-12 13:35:51 +0600

    setup slugify

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -1,13 +1,21 @@
+const slugify = require("slugify");
 const { successResponse } = require("./responseController");
 
 const handleCreateCategory = async (req, res, next) => {
   try {
     const { name } = req.body;
-
+    const slug = slugify(name, {
+      replacement: "-",
+      remove: /[*+~.()'"!:@]/g,
+      lower: true,
+      strict: true,
+      locale: "vi",
+      trim: true,
+    });
     return successResponse(res, {
       statusCode: 201,
       message: "Category created successfully",
-      payload: {name},
+      payload: {name, slug},
     });
   } catch (error) {
     next(error);
