6a8616ccad2d90a644159ffb160e64bb30c68b86
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-11-12 12:13:37 +0600

    refactor app.js

--- server/src/app.js
+++ server/src/app.js
@@ -22,32 +22,16 @@ const limiter = rateLimit({
 
 // middleware
 
-// custom middleware
-const isLoggedIn = (req, res, next) => {
-  const login = true;
-  if (login) {
-    req.body.id = 101;
-    next();
-  } else {
-    console.log("Please login first.");
-    return res
-      .status(401)
-      .json({ message: "user not logged in", status: "Unauthorized (401)" });
-  }
-};
-
 // app.use is application level middleware. its work for all API.
 
-// app.use(isLoggedIn)  // isLoggedIn is a custom middleware to check if user logged in or not.
-
 // Third-party middleware
-app.use(morgan("dev")); // morgan used for getting some information on console when api calling.
+app.use(morgan("dev")); // information on console when api calling.
 
 // built-in middleware
-app.use(express.json()); // express.json parses incoming requests with JSON payloads
-app.use(express.urlencoded({ extended: true })); // express.urlencoded parses incoming requests with URL-encoded payloads (form data)
-app.use(xss()); // Express 4.x middleware which sanitizes user input data (in req.body, req.query, req.headers and req.params) to prevent Cross Site Scripting (XSS) attack. its a alternative for 'xss-clean' which is deprecated. it also can be handled by express-validator
-app.use(limiter); //Use to limit repeated requests to public APIs and/or endpoints such as password reset.
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(xss()); // sanitizes Cross Site Scripting (XSS) attack.  alternative for 'xss-clean' which is deprecated.
+app.use(limiter); 
 app.use(cookieParser()); //handle cookie
 
 // Routes
@@ -58,12 +42,6 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   const text = req.query.text;
   res.status(200).send(`Server is working fine. ${text}`);
-});
-
-// protected route with isLoggedIn middleware
-app.get("/api/profile", isLoggedIn, (req, res) => {
-  const id = req.body.id;
-  res.status(200).send({ id, message: "Welcome to your profile" });
 });
 
 // using router
