bc0b13f5eaffd993120e0ccdeb5908c9e00c100f
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-12-17 17:30:59 +0600

    file upload combining (half)

--- server/src/middlewares/uploadUser.js
+++ server/src/middlewares/uploadUser.js
@@ -4,7 +4,19 @@ const {
   maxImageSize,
   allowedImageExtensions,
   userImagePath,
+  productImagePath,
 } = require("../config/config");
+
+const fileFilter = (req, file, cb) => {
+  const extension = path.extname(file.originalname).substring(1);
+  if (!allowedImageExtensions.includes(extension)) {
+    const error = new Error(
+      `Try allowed image file types (${allowedImageExtensions})`
+    );
+    return cb(error, false);
+  }
+  return cb(null, true);
+};
 
 const userStorage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,19 +30,27 @@ const userStorage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const extension = path.extname(file.originalname).substring(1);
-  if (!allowedImageExtensions.includes(extension)) {
-    const error = new Error(
-      `Try allowed image file types (${allowedImageExtensions})`
-    );
-    return cb(error, false);
-  }
-  return cb(null, true);
-};
+const productStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, productImagePath);
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname);
+    const filename =
+      file.originalname.replace(extension, "") + "_" + Date.now() + extension;
+    cb(null, filename);
+  },
+});
+
 
 const uploadUserImage = multer({
   storage: userStorage,
+  limits: { fileSize: maxImageSize },
+  fileFilter,
+});
+
+const uploadProductImage = multer({
+  storage: productStorage,
   limits: { fileSize: maxImageSize },
   fileFilter,
 });
