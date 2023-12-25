d8a686e318a17f37d194238475dd336ccc98ce45
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-12-25 20:46:34 +0600

    useCloudinary in handleCreateProduct

--- server/src/helper/useCloudinary.js
+++ server/src/helper/useCloudinary.js
@@ -1,7 +1,7 @@
 const createHttpError = require("http-errors");
 const cloudinary  = require("../config/cloudinaryConfig");
 
-const useCloudinary = async (path, subfolder) => {
+const useCloudinary = async (path, subfolder="others") => {
   try {
     const cloudImage = await cloudinary.uploader.upload(path, {
       folder: `ecommerceMern/${subfolder}`,
